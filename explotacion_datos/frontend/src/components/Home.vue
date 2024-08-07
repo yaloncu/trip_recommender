acae041ccca981e4687f382e79b330844b39f945
yaloncu <yaizaalonsocuervo@gmail.com>
2024-08-07 18:45:45 +0200

    cambiando estilos

--- explotacion_datos/frontend/src/components/Home.vue
+++ explotacion_datos/frontend/src/components/Home.vue
@@ -1,10 +1,7 @@
 <template>
-  <section class="hero is-info">
-    <div class="hero-body">
-      <p class="title">Welcome</p>
-      <p class="subtitle">Info subtitle</p>
-    </div>
-  </section>
+  <div class="container">
+    <h1>Welcome</h1>
+  </div>     
 </template>
 
 <script>
@@ -14,4 +11,25 @@ export default {
 </script>
 
 <style scoped>
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+  .container {
+    width: 100%;
+    height: 100vh; 
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    --color: rgba(114, 114, 114, 0.3);
+    background-color: #191a1a;
+    background-image: linear-gradient(0deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%, transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%, transparent),
+        linear-gradient(90deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%, transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%, transparent);
+    background-size: 55px 55px;
+    color: white; 
+    text-align: center; 
+  }
 </style>
+
