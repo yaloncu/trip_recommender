b6b909473896716e516603020217d84aceafd83a
yaloncu <yaizaalonsocuervo@gmail.com>
2025-07-07 18:25:17 +0200

    api key deleted

--- explotacion_datos/frontend/src/firebaseConfig.js
+++ explotacion_datos/frontend/src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBnKZM4SQ8mZ_xWv9s1Qwq_ja6_jwDQ4M",
+  apiKey: "apiKey",
   authDomain: "trip-726fc.firebaseapp.com",
   projectId: "trip-726fc",
   storageBucket: "trip-726fc.appspot.com",
-  messagingSenderId: "871596666349",
-  appId: "1:871596666349:web:96b1d2ce354696a2ee4be4",
-  measurementId: "G-Y54EE5L9V7"
+  messagingSenderId: "messagingSenderId",
+  appId: "appId",
+  measurementId: "measurementId"
 };
 
 // Inicializar Firebase
